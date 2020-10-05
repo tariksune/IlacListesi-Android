d30c2be9ca209608a92b4667ed5dbfaa3ea6a140
tariksune <tariksune@gmail.com>
2020-10-05 22:19:07 +0300

    developing

--- app/src/main/java/com/tarxsoft/ilaclistesi/MainActivity.java
+++ app/src/main/java/com/tarxsoft/ilaclistesi/MainActivity.java
@@ -1,13 +1,19 @@
 package com.tarxsoft.ilaclistesi;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.SearchView;
 
 import com.android.volley.Request;
@@ -16,6 +22,10 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.doubleclick.PublisherAdRequest;
+import com.google.android.gms.ads.doubleclick.PublisherInterstitialAd;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,12 +36,17 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private AdView adView;
+    private PublisherInterstitialAd publisherInterstitialAd;
+
     private static final String TAG = "TAG";
     private static String JSON_URL = "https://805a3cb0dfe6.ngrok.io/api/drugs";
 
     RecyclerView recyclerView;
+    GridLayoutManager gridLayoutManager;
     List<Drugs> drugs;
     Adapter adapter;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,12 +55,15 @@ public class MainActivity extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.drugList);
         drugs = new ArrayList<>();
+        prepareAds();
         listOfDrugs();
     }
 
     private void listOfDrugs() {
 
         RequestQueue requestQueue = Volley.newRequestQueue(this);
+        progressBar = (ProgressBar)findViewById(R.id.progressBar);
+        progressBar.setVisibility(View.VISIBLE);
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, JSON_URL, null, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
@@ -63,13 +81,18 @@ public class MainActivity extends AppCompatActivity {
                         e.printStackTrace();
                     }
                 }
-                recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                progressBar.setVisibility(View.INVISIBLE);
+                gridLayoutManager = new GridLayoutManager(getApplicationContext(),1);
+                recyclerView.setHasFixedSize(true);
+                recyclerView.setLayoutManager(gridLayoutManager);
+                //recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
                 adapter = new Adapter(getApplicationContext(),drugs);
                 recyclerView.setAdapter(adapter);
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
+                progressBar.setVisibility(View.INVISIBLE);
                 Log.d(TAG,"Hata: "+ error.getMessage());
             }
         });
@@ -95,6 +118,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        MenuItem about = menu.findItem(R.id.about);
+        about.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                startActivity(new Intent(getApplicationContext(),AboutActivity.class));
+                Ads();
+                return true;
+            }
+        });
+
         return super.onCreateOptionsMenu(menu);
     }
+
+    private void prepareAds(){
+        adView = findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+        recyclerView.setPadding(0, 0, 0, adView.getHeight());
+        recyclerView.setClipToPadding(false);
+
+    }
+
+    public void Ads() {
+        publisherInterstitialAd = new PublisherInterstitialAd(this);
+        publisherInterstitialAd.setAdUnitId("/6499/example/interstitial");
+        publisherInterstitialAd.loadAd(new PublisherAdRequest.Builder().build());
+        if (publisherInterstitialAd.isLoaded()){
+            publisherInterstitialAd.show();
+        }else{
+            finish();
+        }
+
+    }
+
 }
